1b2e9bb472fd82a7795b5a712d40012fe17eb8ff
João Victor Tinoco <joaovictortinoco@gmail.com>
2022-11-22 13:41:38 +0000

    simple GET endpoint for final project

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express();
+const port = 3000;
+
+app.get('/ping', (req, res) => {
+    res.send({"ping": "pong"})
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
